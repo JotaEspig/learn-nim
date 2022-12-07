827d2ebde6f82fe71c93df860a5a7bbf14a76e1f
João Vitor Espig <jotinha1300@gmail.com>
2022-12-07 18:57:22 -0300

    added control flow

---
+++ basics/controlflow.nim
@@ -0,0 +1,18 @@
+# If statements works similar to python
+if 10 > 6:
+    echo "10 is greater than 6"
+elif 10 < 10:
+    echo "Something is wrong"
+else:
+    echo "Something is world with the world then"
+
+# case is like switch in other languages
+const x: int8 = 10
+case x
+of 5:
+    echo "5"
+of 7:
+    echo "7"
+of 10:
+    echo "10"
+else: discard
