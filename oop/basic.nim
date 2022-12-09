80ac289bd98b31560ccea541b4907d9535712641
João Vitor Espig <jotinha1300@gmail.com>
2022-12-08 23:05:46 -0300

    added more amout oop

--- oop/basic.nim
+++ oop/basic.nim
@@ -1,6 +1,18 @@
-type Person = ref object
-    name*: string
-    age: int16 
+type Person* = ref object
+    name*: string # public
+    agee: int16  # protected/private
 
-var p = Person(name: "Jota", age: 18)
+var p = Person(name: "Jota", agee: 18)
 echo p.name
+
+proc speak*(p: var Person, text: string) =
+    echo p.name & " says: " & text
+
+p.speak "aaaa"
+proc age*(p: var Person): int16 =
+    p.agee
+proc `age=`*(p: var Person, value: int) =
+    p.agee = int16(value)
+
+p.age = 10
+echo p.age
