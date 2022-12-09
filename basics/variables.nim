a693a1e78b87e0ba6e80ea975372d4e9b6323c35
João Vitor Espig <jotinha1300@gmail.com>
2022-12-08 22:12:54 -0300

    added variables

---
+++ basics/variables.nim
@@ -0,0 +1,17 @@
+# Declaration
+var name: string
+name = "Jota"
+echo name
+
+# Also works
+var name2 = "Jota"
+echo name2
+
+# let keyword doesn't allow reassignment
+let name3: string = "Jota"
+# `name3 = "Samuel"` CAUSES AN ERROR]
+echo name3
+
+# const keyword is similar to let, but it needs to assign a value when declarating it
+const name4 = "Jota"
+echo name4
